ae8e6dbd3e7323ce2f2dca3bb369a939289f6430
gleyw <gleyw@DESKTOP-JB62D3J>
2018-07-29 22:41:34 -0300

    Criação dos campos e do método que calcula

--- src/tipcalculator/TipCalculator.java
+++ src/tipcalculator/TipCalculator.java
@@ -23,6 +23,7 @@ public class TipCalculator extends Application {
         
         Scene scene = new Scene(root);
         
+        stage.setTitle("TipCalculator");
         stage.setScene(scene);
         stage.show();
     }
--- src/tipcalculator/TipCalculatorController.java
+++ src/tipcalculator/TipCalculatorController.java
@@ -5,12 +5,16 @@
  */
 package tipcalculator;
 
+import java.math.BigDecimal;
 import java.net.URL;
+import java.text.NumberFormat;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
 
 /**
  *
@@ -18,18 +22,46 @@ import javafx.scene.control.Label;
  */
 public class TipCalculatorController implements Initializable {
     
-    @FXML
-    private Label label;
+    //formata os valores para moedas e percetagens
+    private static final NumberFormat currency = NumberFormat.getCurrencyInstance();
+    private static final NumberFormat percent = NumberFormat.getPercentInstance();
     
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+    private BigDecimal tipPercentage = new BigDecimal(0.15); // 15% padrão
     
+    
+    //controles da Gui definidos na FXML e usados pelo código do controlador
+    @FXML
+    private Label tipPercentageLabel;
+    @FXML
+    private TextField amountTexField;
+    @FXML
+    private TextField tipTextField;
+    @FXML
+    private Slider tipPercentageSlider;
+    @FXML
+    private TextField totalTextField;
+    
+   
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    //calcula e exibe o valor de gorjeta e o valor total
+    @FXML
+    private void calculateButtonPressed(ActionEvent event) {
+        try{
+            BigDecimal amount = new BigDecimal(amountTexField.getText());
+            BigDecimal tip = amount.multiply(tipPercentage);
+            BigDecimal total = total = amount.add(tip);
+            
+            tipTextField.setText(currency.format(tip));
+            totalTextField.setText(currency.format(total));
+        } catch (NumberFormatException ex) {
+            amountTexField.setText("Entre com o valor");
+            amountTexField.selectAll();
+            amountTexField.requestFocus();
+        }
+    }
     
 }
