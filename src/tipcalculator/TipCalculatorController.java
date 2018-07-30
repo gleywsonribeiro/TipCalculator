994ecef83f075c792bb7fc00ab4c28b7d4f5cfaa
Gleywson <Gleywson@hmue00118605>
2018-07-30 13:59:37 -0300

    Criação do método initialize, mas sem sucesso

--- src/tipcalculator/TipCalculatorController.java
+++ src/tipcalculator/TipCalculatorController.java
@@ -6,9 +6,12 @@
 package tipcalculator;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.net.URL;
 import java.text.NumberFormat;
 import java.util.ResourceBundle;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -21,14 +24,13 @@ import javafx.scene.control.TextField;
  * @author Gleywson
  */
 public class TipCalculatorController implements Initializable {
-    
+
     //formata os valores para moedas e percetagens
     private static final NumberFormat currency = NumberFormat.getCurrencyInstance();
     private static final NumberFormat percent = NumberFormat.getPercentInstance();
-    
+
     private BigDecimal tipPercentage = new BigDecimal(0.15); // 15% padrão
-    
-    
+
     //controles da Gui definidos na FXML e usados pelo código do controlador
     @FXML
     private Label tipPercentageLabel;
@@ -40,28 +42,43 @@ public class TipCalculatorController implements Initializable {
     private Slider tipPercentageSlider;
     @FXML
     private TextField totalTextField;
-    
-   
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+
+    //chamado pelo FXMLoader para inicializar o controlador
+    public void initialize() {
+        //0 a 4 arredonda para baixo, 5 a 9 arredonda para cima
+        currency.setRoundingMode(RoundingMode.HALF_UP);
+
+        //ouvinte para alterações no valor do tipPercentageSlider
+        tipPercentageSlider.valueProperty().addListener(
+                (observable, oldValue, newValue) -> {
+                    tipPercentage = BigDecimal.valueOf(newValue.intValue() / 100.0);
+                    tipPercentageLabel.setText(percent.format(tipPercentage));
+                }
+        );
+    }
 
     //calcula e exibe o valor de gorjeta e o valor total
     @FXML
     private void calculateButtonPressed(ActionEvent event) {
-        try{
+        try {
             BigDecimal amount = new BigDecimal(amountTexField.getText());
             BigDecimal tip = amount.multiply(tipPercentage);
-            BigDecimal total = total = amount.add(tip);
-            
+            BigDecimal total = amount.add(tip);
+
             tipTextField.setText(currency.format(tip));
             totalTextField.setText(currency.format(total));
         } catch (NumberFormatException ex) {
             amountTexField.setText("Entre com o valor");
             amountTexField.selectAll();
             amountTexField.requestFocus();
+            totalTextField.setText("");
+            tipTextField.setText("");
         }
     }
-    
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
 }
